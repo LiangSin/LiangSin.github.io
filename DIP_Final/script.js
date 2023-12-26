c7fb71f0289e7f56b4586b5b192c2ad852bfa072
賴亮昕 <liang@lailiangxindeMacBook-Air.local>
2023-12-27 03:32:55 +0800

    button test

--- DIP_Final/script.js
+++ DIP_Final/script.js
@@ -1,5 +1,8 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var image = document.getElementById('myImage');
-    var visibility = image.getAttribute('visible') === 'true';
-    image.setAttribute('visible', !visibility);
-});
+window.onload = function() {
+  document.getElementById('toggleButton').addEventListener('click', function() {
+      console.log("Button clicked"); // 测试按钮点击
+      var image = document.getElementById('myImage');
+      var visibility = image.getAttribute('visible') === 'true';
+      image.setAttribute('visible', !visibility);
+  });
+};
